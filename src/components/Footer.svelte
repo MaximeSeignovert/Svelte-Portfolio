5892b2221b33895c149b5ae007aee84f484ffbad
mseignovert <mseignovert@horizontalsoftware.com>
2024-06-10 14:14:19 +0200

    Modification Footer et title

--- src/components/Footer.svelte
+++ src/components/Footer.svelte
@@ -1,56 +1,17 @@
-<footer>
+<footer class="bg-gray-800 text-white p-5 flex justify-around items-start">
     <div>
-      <h3>Adresse</h3>
-      <p>123 Rue de la Programmation</p>
-      <p>Ville, Pays</p>
+      <h3 class="text-lg mb-[10px]">Adresse</h3>
+      <p class='my-[5px]'>123 Rue de la Programmation</p>
+      <p class='my-[5px]'>Ville, Pays</p>
     </div>
     <div>
-      <h3>Réseaux Sociaux</h3>
-      <ul>
-        <li><a href="#">Github</a></li>
-        <li><a href="#">Linkedin</a></li>
+      <h3 class="text-lg mb-[10px]">Réseaux Sociaux</h3>
+      <ul class="list-none p-0">
+        <li class="mb-[5px] no-underline hover:underline"><a class="text-white" target="_blank" href="https://github.com/MaximeSeignovert">Github</a></li>
+        <li class="mb-[5px] no-underline hover:underline"><a class="text-white" target="_blank" href="https://github.com/MaximeSeignovert">Linkedin</a></li>
       </ul>
     </div>
     <div >
-      <p class="flex flex-row flex-justify cursor-pointer">Made With 🧡 Powered by <img class="w-4 ml-2" alt="Icone Svelte" src="https://upload.wikimedia.org/wikipedia/commons/1/1b/Svelte_Logo.svg"/></p>
+      <p class="flex flex-row flex-justify cursor-pointer my-[5px]">Made With 🧡 Powered by <img class="w-4 ml-2" alt="Icone Svelte" src="https://upload.wikimedia.org/wikipedia/commons/1/1b/Svelte_Logo.svg"/></p>
     </div>
   </footer>
-  
-  <style>
-    footer {
-      background-color: #333;
-      color: white;
-      padding: 20px;
-      display: flex;
-      justify-content: space-around;
-      align-items: flex-start;
-    }
-  
-    h3 {
-      font-size: 1.2em;
-      margin-bottom: 10px;
-    }
-  
-    p {
-      margin: 5px 0;
-    }
-  
-    ul {
-      list-style-type: none;
-      padding: 0;
-    }
-  
-    li {
-      margin-bottom: 5px;
-    }
-  
-    a {
-      color: white;
-      text-decoration: none;
-    }
-  
-    a:hover {
-      text-decoration: underline;
-    }
-  </style>
-  
