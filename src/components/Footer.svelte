7ce2f75ef61a7f1465d17f4f07a03edb36b53fc4
mseignovert <mseignovert@horizontalsoftware.com>
2024-07-02 14:59:23 +0200

    Ajout icones github et linkedin + ajou alt image

--- src/components/Footer.svelte
+++ src/components/Footer.svelte
@@ -1,9 +1,22 @@
+<script>
+    import Fa from 'svelte-fa'
+    import { faGithub, faLinkedin } from '@fortawesome/free-brands-svg-icons'
+</script>
+
 <footer class="w-[100vw] bg-gray-800 text-white p-5 flex justify-around items-center">
     
     <div>
-      <ul class="list-none flex">
-        <li class="px-2 no-underline hover:underline"><a class="text-white" target="_blank" href="https://github.com/MaximeSeignovert">Github</a></li>
-        <li class="px-2 no-underline hover:underline"><a class="text-white" target="_blank" href="https://www.linkedin.com/in/maxime-seignovert-735145317/">Linkedin</a></li>
+      <ul class="list-none flex space-x-4">
+        <a target="_blank" href="https://github.com/MaximeSeignovert" class="text-white flex flex-row items-center">
+          <Fa icon={faGithub} />
+          <li class="px-2 no-underline hover:underline">Github</li>
+        </a>
+        
+        <a target="_blank" href="https://www.linkedin.com/in/maxime-seignovert-735145317/" class="text-white flex flex-row items-center">
+          <Fa icon={faLinkedin} />
+          <li class="px-2 no-underline hover:underline">Linkedin</li>
+        </a>
+
       </ul>
     </div>
     <div >
